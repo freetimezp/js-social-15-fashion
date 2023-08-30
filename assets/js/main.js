f55642f05b8335623dc9c5e65933277bdd42e5cf
freetimezp <kraftukrnet@ukr.net>
2023-08-31 00:47:12 +0300

    13. js-social-15-fashion. Responsive. Toggle menu on mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,10 @@ if (selectHeader) {
 function srollToTop() {
     window.scrollTo(0, 0);
 }
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const navMobile = document.querySelector('.nav-mobile');
+    menu.classList.toggle('active');
+    navMobile.classList.toggle('active');
+}
